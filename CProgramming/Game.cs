fd7c9949dce7b1d36ae789de5fead6784792b40b
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-01-27 21:14:13 +0900

    mini game commit
    1. 컴퓨터가 낸 가위바위보 맞추기
    2. 숫자 야구게임

---
+++ CProgramming/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Solution
+{
+    static void Main(string[] args)
+    {
+
+        //// 1) 가위바위보 맞추기
+        //string[] choices = { "가위", "바위", "보" };
+        //string playerChoices = "";
+        //string computerChoices = choices[new Random().Next(0, 3)];
+
+        //while (playerChoices != computerChoices)
+        //{
+        //    Console.WriteLine("가위, 바위, 보 중 하나를 선택해주세요");
+        //    playerChoices = Console.ReadLine();
+
+        //    if (playerChoices == computerChoices)
+        //    {
+        //        Console.WriteLine("비겼습니다.");
+
+        //    }
+        //    else if ((playerChoices == "가위" && computerChoices == "보") ||
+        //        (playerChoices == "바위" && computerChoices == "가위") ||
+        //        (playerChoices == "보" && computerChoices == "바위"))
+        //    {
+        //        Console.WriteLine("이겼습니다.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("졌습니다.");
+        //    }
+
+        // 2) 숫자 맞추기
+        int targetNumver = new Random().Next(1, 100);
+        int guess = 0;
+        int count = 0;
+        Console.WriteLine("1부터 100 사이이 숫자를 맞춰보세요");
+
+        while(guess != targetNumver)
+        {
+            Console.Write("추측한 숫자를 입력하세요: ");
+            guess = int.Parse(Console.ReadLine());
+            count++;
+
+            if (guess < targetNumver)
+            {
+                Console.WriteLine("좀 더 큰 숫자를 입력해주세요");
+            }
+            else if (guess > targetNumver)
+            {
+                Console.WriteLine("좀 더 작은 숫자를 입력해주세요");
+            }
+            else
+            {
+                Console.WriteLine("축하합니다. 숫자를 맞추셨습니다.");
+                Console.WriteLine("시도한 횟수: " + count);
+            }
+        }
+    }
+}
---
+++ Conditional statements and loops/loopCode.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+public class Solution
+{
+    static void Main(string[] args)
+    {
+        // for(초기식, 조건식, 증감식)
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+
+        int ii = 0;
+        while (ii < 10)
+        {
+            Console.WriteLine(ii);
+            ii++;
+        }
+
+        // 최초의 한번을 허용 할 것이가?
+        int sum = 0;
+        int num;
+
+        do
+        {
+            Console.Write("숫자를 입력하세요 (0 입력 시 종료): ");
+            num = int.Parse(Console.ReadLine());
+            sum += num;
+        } while (num != 0);
+
+        Console.WriteLine("합계: " + sum);
+
+        Console.WriteLine();
+
+        // 데이터 기준으로 돌기
+        string[] inventory = { "검", "방패", "활", "화살", "물약" };
+
+        foreach (string item in inventory)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.WriteLine("i: {0}, j: {1}", i, j);
+            }
+        }
+        Console.WriteLine();
+
+        for (int i = 2; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.WriteLine(i + " x " + j + " = " + (i * j));
+            }
+        }
+
+        // 가로 구구단
+        for (int i = 2; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write(i + " x " + j + " = " + (i * j) + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        // 세로 구구단
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 2; j <= 9; j++)
+            {
+                Console.Write(j + " x " + i + " = " + (i * j) + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 3 == 0)
+            {
+                continue; // 3의 배수일 경우 다음 숫자로 넘어감
+            }
+
+            Console.WriteLine(i);
+            if (i == 7)
+            {
+                break; // 7이 출력된 이후에는 반복문을 빠져나감
+            }
+        }
+
+        //int sum = 0;
+
+        while (true)
+        {
+            Console.Write("숫자를 입력하세요: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
+            {
+                Console.WriteLine("프로그램을 종료합니다.");
+                break;
+            }
+
+            if (input < 0)
+            {
+                Console.WriteLine("음수는 무시합니다.");
+                continue;
+            }
+
+            sum += input;
+            Console.WriteLine("현재까지의 합: " + sum);
+        }
+
+        Console.WriteLine("합계: " + sum);
+    }
+}
