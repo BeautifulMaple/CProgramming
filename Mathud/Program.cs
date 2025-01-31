307e993af9085296de6f561a8ea27897a368f6bc
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-01-31 09:30:37 +0900

    재귀함수와 오버로딩
    - 오버라이딩에 대해서 찾아보기

---
+++ Mathud/Program.cs
@@ -0,0 +1,32 @@
+﻿class Mathud
+{
+    static void PrintLine()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("=");
+        }
+        Console.WriteLine();
+    }
+
+    static void PrintLine2(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("=");
+        }
+        Console.WriteLine();
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    static void Main (String[] args)
+    {
+        PrintLine();
+        PrintLine2(20);
+        int result = Add(10, 20);
+        Console.WriteLine(result);
+    }
+}
---
+++ Overloding/Program.cs
@@ -0,0 +1,25 @@
+﻿class Overloding
+{
+    static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+    static int AddNumbers(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    static float AddNumbers(float a, float b, float c)
+    {
+        return a + b + c;
+    }
+    static void Main(string[] args)
+    {
+        // 메서드 호출
+        int sum1 = AddNumbers(10, 20);  // 두 개의 정수 매개변수를 가진 메서드 호출
+        int sum2 = AddNumbers(10, 20, 30);  // 세 개의 정수 매개변수를 가진 메서드 호출
+        float sum3 = AddNumbers(10, 20, 30);
+
+        Console.WriteLine(sum1);
+    }
+}
