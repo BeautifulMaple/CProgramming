e78f2aab4d40379bcc93a179cfc2de9de0e8dc35
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-01-31 15:21:22 +0900

    추상 클래스와 메서드
    오버라이딩: 함수를 덮어쓰는 것
    오버로딩 : 함수를 읽는 것

--- AbstractClass/Program.cs
+++ AbstractClass/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
+    //추상 클래스
+    abstract class Shape
+    {
+        // 추상 메소드 틀만 재공
+        public abstract void Draw();
+    }
+
+    class Circle : Shape
+    {
+        // 강재로 클에 속을 부여
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+    }
+
+    class Square : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Square");
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Triangle");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Shape> list = new List<Shape>();
+            list.Add(new Circle());
+            list.Add(new Triangle());
+            list.Add(new Square());
+
+            foreach (Shape shape in list)
+            {
+                shape.Draw();
+            }
         }
     }
 }
