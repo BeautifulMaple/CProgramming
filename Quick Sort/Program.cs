bedee37d08694f51e757ec965119f07049e357a9
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-02-03 21:02:49 +0900

    Commit

---
+++ Quick Sort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quick_Sort
+{
+    
+    internal class Program
+    {
+        static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        static int Partition(int[] arr, int left, int right)
+        {   // { 5,2, 4, 3, 0,  1 };
+            int pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                {
+                    if (arr[j] < pivot)
+                    {
+                        i++;
+                        Swap(arr, i, j);
+                    }
+                }
+            }
+            Swap(arr, i + 1, right);
+
+            return i +  1;
+        }
+
+        static void QuickSort(int[] arr, int left, int right)
+        {
+            if(left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                QuickSort(arr, pivot, right);
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5,2, 4, 3, 0,  1 };
+            QuickSort(arr, 0, arr.Length - 1);
+
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
--- sorting algorithm/Program.cs
+++ sorting algorithm/Program.cs
@@ -10,6 +10,25 @@ namespace sorting_algorithm
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[] { 1, 4, 5, 6, 7, 2, 3, 8, 9, 10, 11,};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+            foreach(int num in arr)
+                Console.WriteLine(num);
         }
     }
 }
