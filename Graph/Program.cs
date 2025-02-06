9df4d034a6df5c31b1c3b7e7bb242b687d4f8c0e
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-02-06 14:14:47 +0900

    commit

---
+++ Graph/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class Graph
+    {
+        private int V;
+        private List<int>[] list;
+
+        public Graph(int v) 
+        { 
+            V = v;
+            list = new List<int>[V];
+            for (int i = 0; i < V; i++)
+            {
+                list[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            list[v].Add(w);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(6);
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(4, 5);
+        }
+    }
+}
---
+++ Text RPG/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_RPG
+{
+    internal class Player
+    {
+        public int Level { get; }    // 캐릭터 레벨
+        public string Name { get; }  // 캐릭터 이름
+        public string Job { get; }   // 캐릭터 직업
+        public int Attack { get; }   // 기본 공격력
+        public int EquiAttack { get; set; }   // 기본 공격력
+        public int Defense { get; }  // 기본 방어력
+        public int EquiDefense { get; set; }  // 기본 방어력
+        public int Health { get; set; }   // 체력
+        public int MaxHealth { get; set; }   // 체력
+
+        public int Gold { get; set; }  // 보유 골드
+
+        public Player(int level, string name, string job, int attack, int equiAttack, int defense, int equiDefense, int health, int maxHealth, int gold)
+        {
+            Level = level;
+            Name = name;
+            Job = job;
+            Attack = attack;
+            EquiAttack = equiAttack;
+            Defense = defense;
+            EquiDefense = equiDefense;
+            Health = health;
+            MaxHealth = maxHealth;
+            Gold = gold;
+        }
+    }
+}
